a20069d6975932ab55f4b53bc68039650954fa99
ranier-codegourmet <ranier@codegourmet.io>
2023-03-04 02:46:59 +0800

    fix(auth-api): remove unused files

--- services/bid-api/src/proto/generated-types/bid/BidStatusResponse.ts
+++
@@ -1,9 +0,0 @@
-// Original file: src/proto/bid.proto
-
-export interface BidStatusResponse {
-  message?: string;
-}
-
-export interface BidStatusResponse__Output {
-  message: string;
-}
--- services/bid-api/src/proto/interface/bid.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from 'rxjs';
-
-import { BidStatusRequest } from '../generated-types/bid/BidStatusRequest';
-import { BidStatusResponse__Output } from '../generated-types/bid/BidStatusResponse';
-
-export interface BidRcpClient {
-  bidStatus: (payload: BidStatusRequest) => Observable<BidStatusResponse__Output>;
-}
